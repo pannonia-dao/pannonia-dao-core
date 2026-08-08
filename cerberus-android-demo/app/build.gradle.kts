d04e264967067164a99ef70f1eb2a5bfa7fe28f0
Lucifer <krizsogergely@gmail.com>
2026-08-08 11:34:03 +0200

    Bump Cerberus Android to v0.3

--- cerberus-android-demo/app/build.gradle.kts
+++ cerberus-android-demo/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
         applicationId = "ai.cerberus.demo"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 }
